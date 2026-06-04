e9ead435171b964d6f85b994b88d09779057be15
pranavkdileep <pranavdileep10@gmail.com>
2026-06-04 13:15:36 +0530

    docker

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { withWorkflow } from "workflow/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
 
 export default withWorkflow(nextConfig);
